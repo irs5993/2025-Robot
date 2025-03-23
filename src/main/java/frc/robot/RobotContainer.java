06bf6fbc1d176ff523ec17b121846e07af204f60
team-irs5993 <91738310+team-irs5993@users.noreply.github.com>
2025-03-23 18:30:53 +0300

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,22 +9,19 @@ import static edu.wpi.first.units.Units.*;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.events.EventTrigger;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.AlignState;
 import frc.robot.Constants.RobotState;
 import frc.robot.commands.AlignReef;
-import frc.robot.commands.DriveDistance;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.ElbowSubsystem;
@@ -69,14 +66,7 @@ public class RobotContainer {
         public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain(visionSubsystem);
 
         public RobotContainer() {
-                // autoChooser = AutoBuilder.buildAutoChooser("Tests"); 
-                // SmartDashboard.putData("Auto Mode", autoChooser);
-
-                // Build an auto chooser. This will use Commands.none() as the default option.
                 autoChooser = AutoBuilder.buildAutoChooser();
-
-                // Another option that allows you to specify the default auto by its name
-                // autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
 
                 SmartDashboard.putData("Auto Chooser", autoChooser);
 
@@ -85,10 +75,6 @@ public class RobotContainer {
         }
 
         private void configureAuto() {
-
-                // var exampleAutoCommand = new PathPlannerAuto("Test Auto");
-                // exampleAutoCommand.event("raiseArm").onTrue(robotHandler.request(RobotState.CORAL_L2));
-
                 new EventTrigger("raiseArm").onTrue(robotHandler.request(RobotState.CORAL_L2));
         }
 
@@ -102,15 +88,11 @@ public class RobotContainer {
 
                                 ));
 
-                // Snake mode
-                // ps5Controller.L2()
-                // .whileTrue(drivetrain.applyRequest(() ->
-                // drive.withVelocityX(-ps5Controller.getLeftY() *
-                // MaxSpeed * 0.35)
-                // .withVelocityY(-ps5Controller.getLeftX() * MaxSpeed * 0.35)
-                // .withRotationalRate(-ps5Controller.getRightX() * MaxAngularRate * 0.35)
-
-                // ));
+                // Tipping protection
+                // new Trigger(() -> Math.abs(drivetrain.getPigeon2().getPitch().getValueAsDouble()) > 15)
+                //                 .onTrue(robotHandler.request(RobotState.ZERO));
+                // new Trigger(() -> Math.abs(drivetrain.getPigeon2().getRoll().getValueAsDouble()) > 15)
+                //                 .onTrue(robotHandler.request(RobotState.ZERO));
 
                 // Odometry reset
                 ps5Controller.options().onTrue(drivetrain.runOnce(() -> {
@@ -206,12 +188,10 @@ public class RobotContainer {
                                 Commands.run(() -> rollerSubsystem.setVoltage(4.5),
                                                 rollerSubsystem));
                 flightPad.button(1).onTrue(
-                                robotHandler.request(RobotState.MAX)
-                );
+                                robotHandler.request(RobotState.MAX));
                 flightPad.button(2).whileTrue(
                                 Commands.runEnd(() -> rollerSubsystem.setVoltage(-2.5), () -> rollerSubsystem.stop(),
                                                 rollerSubsystem));
-
 
         }
 
